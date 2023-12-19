909fd5b995ed6a2bc672ab961dc84f90b4cad454
biless <biless@qq.com>
2023-12-20 00:32:21 +0800

    Update notify.groovy

--- src/github/biless/notify.groovy
+++ src/github/biless/notify.groovy
@@ -1,34 +1,34 @@
 package github.biless
 
-// def SendWxWork(Status,CatchInfo=' ') {
-//   echo "!--------- Send WxWork......"
-// 	sh """
-// 		curl --location --request POST 'https://qyapi.weixin.qq.com/cgi-bin/webhook/send?key=$appToken' \
-// 		--header 'Content-Type: application/json' \
-// 		--data '{
-//             "msgtype": "markdown",
-//             "markdown": {
-//               "content": "### 生产发布申请,请【${adminUser}】审批@${approvalDD}\n- 应用名称: ${env.JOB_NAME}\n- 构建信息: ${Status}${CatchInfo}\n- 随机验证码: ${randomToken}",
-//               "mentioned_list": [ "@all" ]
-//             }
-//         }'
-// 	"""
-// }
-
-// 发送审批信息
-def SendApprovalWxWork(AppToken,ApprovalUser,JobName,Info,ApprovalCode = '') {
-  echo "!--------- Send WxWork --------------"
+def SendWxWork(Status,CatchInfo=' ') {
+  echo "!--------- Send WxWork......"
 	sh """
-		curl --location --request POST 'https://qyapi.weixin.qq.com/cgi-bin/webhook/send?key=$AppToken' \
+		curl --location --request POST 'https://qyapi.weixin.qq.com/cgi-bin/webhook/send?key=$appToken' \
 		--header 'Content-Type: application/json' \
 		--data '{
             "msgtype": "markdown",
             "markdown": {
-              "content": "### 【${ApprovalUser}】发布上线申请,请审批\n> 应用名称: ${JobName}\n> 构建信息: ${Info}\n> 随机验证码: ${ApprovalCode}\n <@all>"
+              "content": "### 生产发布申请,请【${adminUser}】审批@${approvalDD}\n- 应用名称: ${env.JOB_NAME}\n- 构建信息: ${Status}${CatchInfo}\n- 随机验证码: ${randomToken}",
+              "mentioned_list": [ "@all" ]
             }
         }'
 	"""
 }
+
+// 发送审批信息
+// def SendApprovalWxWork(AppToken,ApprovalUser,JobName,Info,ApprovalCode = '') {
+//   echo "!--------- Send WxWork --------------"
+// 	sh """
+// 		curl --location --request POST 'https://qyapi.weixin.qq.com/cgi-bin/webhook/send?key=$AppToken' \
+// 		--header 'Content-Type: application/json' \
+// 		--data '{
+//             "msgtype": "markdown",
+//             "markdown": {
+//               "content": "### 【${ApprovalUser}】发布上线申请,请审批\n> 应用名称: ${JobName}\n> 构建信息: ${Info}\n> 随机验证码: ${ApprovalCode}\n <@all>"
+//             }
+//         }'
+// 	"""
+// }
 
 // 发送上线信息
 // def SendStatusWxWork(appToken,approvalUser,status,jobName) {
