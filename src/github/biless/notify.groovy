93ec4f81c9d698ae16878e62e3c5269217539419
biless <biless@qq.com>
2023-12-19 12:06:19 +0800

    Update notify.groovy

--- src/github/biless/notify.groovy
+++ src/github/biless/notify.groovy
@@ -14,3 +14,45 @@ def SendWxWork(Status,CatchInfo=' ') {
         }'
 	"""
 }
+
+// 发送审批信息
+def SendApprovalWxWork(appToken,approvalUser,jobName,info,approvalCode) {
+  echo "!--------- Send WxWork --------------"
+	sh """
+		curl --location --request POST 'https://qyapi.weixin.qq.com/cgi-bin/webhook/send?key=$appToken' \
+		--header 'Content-Type: application/json' \
+		--data '{
+            "msgtype": "markdown",
+            "markdown": {
+              "content": "### 【${approvalUser}】发布上线申请,请审批\n> 应用名称: ${jobName}\n> 构建信息: ${info}\n> 随机验证码: ${approvalCode}\n <@all>",
+              "mentioned_list": [ "@all" ]
+            }
+        }'
+	"""
+  }
+
+// 发送上线信息
+def SendStatusWxWork(appToken,approvalUser,status,jobName) {
+  statusDes = "<font color=\"info\">已完成</font>"
+  status = status ?: "failure"
+  if (status = "success") {
+    
+  } else if (status = "failure") {
+    statusDes = "<font color=\"warning\">失败</font>"
+  } else {
+    statusDes = "<font color=\"comment\">已取消</font>"
+  }
+
+  echo "!--------- Send WxWork --------------"
+	sh """
+		curl --location --request POST 'https://qyapi.weixin.qq.com/cgi-bin/webhook/send?key=$appToken' \
+		--header 'Content-Type: application/json' \
+		--data '{
+            "msgtype": "markdown",
+            "markdown": {
+              "content": "### 【${approvalUser}】发布上线申请\n> 应用名称: ${jobName}\n> 构建结果: ${statusDes}\n <@all>",
+              "mentioned_list": [ "@all" ]
+            }
+        }'
+	"""
+  }
