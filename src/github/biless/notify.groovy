22111e13080d2e976449ce0bdfbabe1742ea7142
biless <biless@qq.com>
2023-12-19 12:21:45 +0800

    Update notify.groovy

--- src/github/biless/notify.groovy
+++ src/github/biless/notify.groovy
@@ -32,27 +32,27 @@ def SendApprovalWxWork(AppToken,ApprovalUser,JobName,Info,ApprovalCode) {
   }
 
 // 发送上线信息
-def SendStatusWxWork(appToken,approvalUser,status,jobName) {
-  statusDes = "<font color=\"info\">已完成</font>"
-  status = status ?: "failure"
-  if (status == "success") {
-    
-  } else if (status == "failure") {
-    statusDes = "<font color=\"warning\">失败</font>"
-  } else {
-    statusDes = "<font color=\"comment\">已取消</font>"
-  }
-
-  echo "!--------- Send WxWork --------------"
-	sh """
-		curl --location --request POST 'https://qyapi.weixin.qq.com/cgi-bin/webhook/send?key=$appToken' \
-		--header 'Content-Type: application/json' \
-		--data '{
-            "msgtype": "markdown",
-            "markdown": {
-              "content": "### 【${approvalUser}】发布上线申请\n> 应用名称: ${jobName}\n> 构建结果: ${statusDes}\n <@all>",
-              "mentioned_list": [ "@all" ]
-            }
-        }'
-	"""
-  }
+// def SendStatusWxWork(appToken,approvalUser,status,jobName) {
+//   statusDes = "<font color=\"info\">已完成</font>"
+//   status = status ?: "failure"
+//   if (status == "success") {
+//
+//   } else if (status == "failure") {
+//     statusDes = "<font color=\"warning\">失败</font>"
+//   } else {
+//     statusDes = "<font color=\"comment\">已取消</font>"
+//   }
+//
+//   echo "!--------- Send WxWork --------------"
+// 	sh """
+// 		curl --location --request POST 'https://qyapi.weixin.qq.com/cgi-bin/webhook/send?key=$appToken' \
+// 		--header 'Content-Type: application/json' \
+// 		--data '{
+//             "msgtype": "markdown",
+//             "markdown": {
+//               "content": "### 【${approvalUser}】发布上线申请\n> 应用名称: ${jobName}\n> 构建结果: ${statusDes}\n <@all>",
+//               "mentioned_list": [ "@all" ]
+//             }
+//         }'
+// 	"""
+//   }
